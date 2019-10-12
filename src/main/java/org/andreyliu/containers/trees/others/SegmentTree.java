c79726ac162258ab87c849e5f13b4dab7e4d962c
andreyliu <yuqiliu@andrew.cmu.edu>
2019-10-12 16:35:47 -0400

    full seg tree impl

--- src/main/java/org/andreyliu/containers/trees/others/SegmentTree.java
+++ src/main/java/org/andreyliu/containers/trees/others/SegmentTree.java
@@ -57,6 +57,7 @@ public class SegmentTree {
         }
         int size = (int) Math.pow(2, IntMath.log2(n, RoundingMode.CEILING));
         this.heap = new Node[size];
+        buildHeap(1, 0, n - 1);
     }
 
     // v starts from 1
@@ -83,32 +84,103 @@ public class SegmentTree {
     }
     /**
      * Performs a range sum query.
+     * From and to are zero-indexed.
      * @param from left bound, inclusive.
      * @param to right bound, inclusive.
      * @return range sum of the data
      */
     public int rsq(int from, int to) {
+        return rsq(1, from, to);
+    }
+    // v starts with 1.
+    private int rsq(int v, int from, int to){
+        Node h = heap[v];
+        if (!h.intersects(from, to)) {
+            return 0;
+        }
+        // we first check for pending values
+        if (h.pendingVal != null && h.contains(from, to)) {
+            return (from - to + 1) * h.pendingVal;
+        }
+        // then check if h matches the interval query
+        if (h.isContainedIn(from, to)) {
+            return h.sum;
+        }
+        propagate(v);
+        int leftSum = rsq(v << 1, from, to);
+        int rightSum = rsq(v << 1 | 1, from, to);
+        return leftSum + rightSum;
+    }
+    // should only be called with non leaf nodes.
+    private void propagate(int v) {
+        Node h = heap[v];
 
+        if (h.pendingVal == null) return;
 
+        change(heap[v << 2], h.pendingVal);
+        change(heap[v << 2 | 1], h.pendingVal);
+        arr[h.from] = h.pendingVal;
+        h.pendingVal = null;
+    }
+
+    private void change(Node h, int val) {
+        h.pendingVal = val;
+        h.sum = h.size() * val;
+        h.min = val;
     }
     /**
      * Performs a range min query.
+     * From and to are zero-indexed.
      * @param from left bound, inclusive.
      * @param to right bound, inclusive.
-     * @return range min of the data
+     * @return range min of the data. When input interval is not in the tree, return Integer.MAX_VALUE
+     *
      */
     public int rMinQ(int from, int to) {
+        return rMinQ(1, from, to);
+    }
 
-
+    private int rMinQ(int v, int from, int to) {
+        Node h = heap[v];
+        if (!h.intersects(from, to)) {
+            return Integer.MAX_VALUE;
+        }
+        if (h.contains(from, to) || h.isContainedIn(from, to)) {
+            return h.min;
+        }
+        propagate(v);
+        int leftMin = rMinQ(v << 1, from, to);
+        int rightMin = rMinQ(v << 1 | 1, from, to);
+        return Math.min(leftMin, rightMin);
     }
     /**
      * Performs a range update on the data. Sets the range of data specified
      * by from and to to the given value.
+     * From and to are zero-indexed.
      * @param from left bound, inclusive.
      * @param to right bound, inclusive.
      * @param value value
      */
-    public void update(int from, int to, Integer value) {
+    public void update(int from, int to, int value) {
+        update(1, from, to, value);
+    }
 
+    private void update(int v, int from, int to, int value) {
+        Node h = heap[v];
+        if (!h.intersects(from, to)) {
+            return;
+        }
+        if (h.isContainedIn(from, to)) {
+            // if h is contained update range, lazily update h.
+            change(h, value);
+            return;
+        }
+        // propagate previous updates that might not completely
+        // overlap with this update
+        propagate(v);
+        update(v << 1, from, to, value);
+        update(v << 1 | 1, from, to, value);
+        h.sum = heap[v << 1].sum + heap[v << 1 | 1].sum;
+        h.min = Math.min(heap[v << 1].min, heap[v << 1 | 1].min);
     }
 }
